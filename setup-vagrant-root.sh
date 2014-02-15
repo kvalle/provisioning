9c250eefd9a30631d07d6548a5c1154c0888874e
Kjetil Valle <kjetil.valle@gmail.com>
2014-02-15 13:51:50 +0100

    script for making vagrant image ready for provisioning

---
+++ setup-vagrant-root.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+## Script for setting root password and configuring
+## ssh authorized_keys with vagrant image.
+## 
+## Run this script with new vagrant images in order
+## to test ansible scripts.
+
+ssh vagrant@192.168.33.10 "sudo passwd root"
+pushd ~/.ssh
+ssh-copy-id -i root@192.168.33.10
+echo -e "All done. Test using:\nssh root@192.168.33.10\n"
+
+popd
