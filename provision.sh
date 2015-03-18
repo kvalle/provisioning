1326e7a65cb0547d54853bc2894ff3d181f073d5
Kjetil Valle <kjetil.valle@gmail.com>
2015-03-18 22:01:37 +0100

    Improved provision script a wee bit

--- provision.sh
+++ provision.sh
@@ -1,12 +1,37 @@
 #!/usr/bin/env bash
-
 set -e
 
-DEFAULT="vagrant"
-echo -en "Which hosts file? [$DEFAULT] \n> "
-read HOST
+function _colored() {
+	COLOR=${1?}
+	TEXT=${2}
 
-echo -en "Anything else? \n> "
-read EXTRA_ARGS
+	if tty -s; then
+		echo -e "\033[${COLOR}m${TEXT}\033[0m";
+	else
+		echo "${TEXT}"
+	fi
+}
 
-ansible-playbook -i inventory/${HOST:-$DEFAULT} server.yml --ask-sudo-pass $EXTRA_ARGS
+function _confirm() {
+	TEXT=${1?}
+	read -p "${TEXT} [y/N]" -n 1 -r; echo
+	[[ $REPLY =~ ^[Yy]$ ]]
+}
+
+function _print() { 
+	_colored "1;37" "$1" 
+}
+
+read -e -i "server.yml" -p "Which playbook: " PLAYBOOK
+read -e -i "vagrant" -p "Which host: " ENV
+read -e -i "" -p "Extra arguments: " ARGS
+
+CMD="ansible-playbook $PLAYBOOK -i inventory/$ENV --ask-sudo-pass $ARGS"
+
+_print ">> $CMD"
+_confirm "Run?" || {
+	_print "Aborted."
+	exit 1
+}
+
+eval "${CMD}"
